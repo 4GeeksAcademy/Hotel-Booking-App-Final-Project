074178fbfa9fc52398320347ed3cd45125508123
María José Fonseca Castro <mfonsecac052@gmail.com>
2025-02-13 20:55:20 +0000

    mejoras en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,14 +5,15 @@ import "../../styles/footer.css";
 export const Footer = () => (
 	<footer className="navBarfooterConfig footer mt-auto py-3 text-center">
 		<div className="footer-content">
-			<p className="contact-title">
-				{ }
-				<Link to="/contact" className="contact-link FontDesign fw-bold">
-					Contact Us
-				</Link>
+			<p className="mb-3 fw-bold FontDesign text-warning">Contact Us</p>
+			<p className="mb-3 FontDesign">
+				<i className="bi bi-envelope me-2"></i>
+				<span>support@sereniahotels.com</span>
 			</p>
-			<p className="contact-email">support@sereniahotels.com</p>
-			<p className="app-name">Hotel Booking App</p>
+			<p className="app-name FontDesign fw-bold mb-0">Hotel Booking App</p>
+			<hr className="bg-light my-4" />
+			<p className="small mb-0">&copy; 2025 Serenia Hotels. All rights reserved.</p>
+
 		</div>
 	</footer>
 );
